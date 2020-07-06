dbb9c540abf84add75a69c578e98b7327e843ad7
GysevOV <gysevov@yandex.ru>
2020-07-06 15:47:53 +0300

    add model Article

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+
+    class Meta:
+        verbose_name = 'Статья'
+
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    subject = models.CharField('Заголовок', max_length=150)
+    text = models.TextField('Текст статьи')
+    publication_date = models.DateTimeField('', auto_now=True)
+
+    def get_short_text(self):
+        return self.text[:250]
